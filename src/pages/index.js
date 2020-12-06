844e56edcfdb98affc67d95002055e781c0b11a6
Youngjae <limyoungjae@gmail.com>
2020-12-05 21:58:39 -0500

    Added styled css in index.js and added images on the list of all blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,45 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
+import Img from "gatsby-image"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const Post = styled.div`
+  display: flex;
+  border: 2px solid #6687D0;
+  border-radius: 10px;
+  padding: 5px;
+  align-items: center;
+
+  :hover {
+    transform: scale(1.02);
+    transition: transform 0.4s ease-out;
+  }
+`
+
+const PostImage = styled.div`
+  flex: 25%;
+  margin-right: 1rem;
+
+  Img {
+    border-radius: 10px;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`
+
+const PostText = styled.div`
+  flex: 75%;
+`
+
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allContentfulPost.edges
-
-  console.log(posts)
 
   if (posts.length === 0) {
     return (
@@ -29,7 +59,7 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      <ol style={{ listStyle: `none` }}>
+      <ol style={{ listStyle: `none`, marginLeft: "0" }}>
         {posts.map(post => {
           const title = post.node.title || post.node.slug
 
@@ -40,17 +70,26 @@ const BlogIndex = ({ data, location }) => {
                 itemScope
                 itemType="http://schema.org/Article"
               >
-                <header>
-                  <h2>
-                    <Link to={post.node.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.node.date}</small>
-                </header>
-                <section>
-                  <p>{post.node.subtitle}</p>
-                </section>
+                <Link
+                  to={post.node.slug}
+                  itemProp="url"
+                  style={{ textDecoration: "none" }}
+                >
+                  <Post key={post.node.slug}>
+                    <PostImage>
+                      <Img fluid={post.node.image.fluid} />
+                    </PostImage>
+                    <PostText>
+                      <header>
+                        <h2>{title}</h2>
+                        <small>{post.node.date}</small>
+                      </header>
+                      <section>
+                        <p>{post.node.subtitle}</p>
+                      </section>
+                    </PostText>
+                  </Post>
+                </Link>
               </article>
             </li>
           )
@@ -77,6 +116,11 @@ export const pageQuery = graphql`
           author
           slug
           date(formatString: "MMMM DD, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
         }
       }
     }
